e4fd7fc9320930fd837e4fc8f9817a807b16a0ed
Murat Oralbaev <successmanonly@gmail.com>
2023-03-28 15:21:56 +0300

    Pom.xml modified

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -3,23 +3,25 @@ import org.junit.jupiter.api.Assertions;
 
 public class BonusServiceTest {
     @Test
-    void shouldCalculateForRegisteredAndUnderLimit () {
+    void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
-        long actual = service.calculate(amount,registered);
+        long actual = service.calculate(amount, registered);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldcalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
         long amount = 1_000_000;
         boolean registered = true;
         long expected = 500;
-        long actual = service.calculate(amount,registered);
+        long actual = service.calculate(amount, registered);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateForNotRegisteredUnderLimit() {
         BonusService service = new BonusService();
@@ -29,6 +31,7 @@ public class BonusServiceTest {
         long actual = service.calculate(amount, registered);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateForNotRegisteredOverLimit() {
         BonusService service = new BonusService();
